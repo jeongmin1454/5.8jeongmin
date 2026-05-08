3a0ca7a799df8dfca52dd0b424f39d15039d2b83
jeongmin1454 <wjdals8855@gmail.com>
2026-05-08 17:58:58 +0900

    Fix JSON file paths and display histogram
    Updated file paths for JSON data and added missing st.pyplot() call.

--- 5.8jeongmin3.py
+++ 5.8jeongmin3.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-df_a = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_a.json')
-df_b = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_b.json')
+df_a = pd.read_json('\heart_failure_a.json')
+df_b = pd.read_json('\heart_failure_b.json')
 df = pd.merge(df_a, df_b, on='person_id', how='inner')
 
 st.title('심박출 슬라이더')
