0a6ad212e31f8ab409d731a7b0c23d92ae63731d
jeongmin1454 <wjdals8855@gmail.com>
2026-05-08 17:30:59 +0900

    Add files via upload

---
+++ 5.8jeongmin2.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df_a = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_a.json')
+df_b = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_b.json')
+df = pd.merge(df_a, df_b, on='person_id', how='inner')
+
+
+
+st.title('흡연 여부 그래프')
+
+smoking_option = st.radio(
+    "흡연?",
+    ('흡연 (1)', '비흡연 (0)')
+)
+
+if smoking_option == '흡연 (1)':
+    filtered_df = df[df['smoking'] == 1]
+else:
+    filtered_df = df[df['smoking'] == 0]
+
+if filtered_df.empty:
+    st.warning("선택된 조건에 해당하는 데이터가 없습니다.")
+else:
+    fig, ax = plt.subplots(figsize=(10, 6)) # plt.figure 대신 권장되는 방식
+    
+
+    sns.violinplot(data=filtered_df, x='DEATH_EVENT', y='platelets', ax=ax)
+    
+    ax.set_title(f'사망 여부와 혈소판 수치 ({smoking_option})')
+    ax.set_xlabel('사망 여부 (DEATH_EVENT)')
+    ax.set_ylabel('혈소판 수치 (platelets)')
+
+
+    st.pyplot(fig)
---
+++ 5.8jeongmin3.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df_a = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_a.json')
+df_b = pd.read_json(r'C:\Users\KDT19\Desktop\KDT-14\과제\5.8박정민\heart_failure_b.json')
+df = pd.merge(df_a, df_b, on='person_id', how='inner')
+
+st.title('심박출 슬라이더')
+
+min_ejection = int(df['ejection_fraction'].min())
+max_ejection = int(df['ejection_fraction'].max())
+
+ejection_range = st.slider(
+    '심박출(ejection_fraction) 범위를 선택하세요:',
+    min_value=min_ejection,
+    max_value=max_ejection,
+    value=(min_ejection, max_ejection) 
+)
+
+
+filtered_df = df[
+    (df['ejection_fraction'] >= ejection_range[0]) & 
+    (df['ejection_fraction'] <= ejection_range[1])
+]
+
+
+if filtered_df.empty:
+    st.warning("선택된 심박출 범위에 해당하는 데이터가 없습니다.")
+else:
+
+    fig = plt.figure(figsize=(10, 8))
+    sns.histplot(data=filtered_df, x='time', bins=20, hue='DEATH_EVENT', kde=True) 
+    plt.title(f' {ejection_range[0]} - {ejection_range[1]})')
+    plt.xlabel('time')
+    plt.ylabel('Count')
+
+    st.pyplot(fig)
