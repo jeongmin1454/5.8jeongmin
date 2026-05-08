f546b6ce37c0506b1cb01fbfe1c15569908cdc3d
jeongmin1454 <wjdals8855@gmail.com>
2026-05-08 17:43:22 +0900

    Add files via upload

---
+++ 5.8jeongmin.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df_a = pd.read_json('./heart_failure_a.json')
+df_b = pd.read_json('./heart_failure_b.json')
+df = pd.merge(df_a, df_b, on='person_id', how='inner')
+
+st.title('박출계수 / 나이')
+
+
+
+g = sns.jointplot(data=df, x='ejection_fraction', y='age', hue='DEATH_EVENT')
+
+st.pyplot(g.fig)
